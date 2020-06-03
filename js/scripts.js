81e18758f4c02e4927d80f364e81d1cb51e7b688
ziziw <ziwangchen@hotmail.com>
2020-06-02 23:25:39 -0400

    add search bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+const body = document.querySelector('body');
 const galleryDiv = document.querySelector('#gallery');
+const searchDiv = document.querySelector('.search-container');
+
+const searchForm = document.createElement('form');
+const searchInput = document.createElement('input');
+const submitBut = document.createElement('input');
+
+searchForm.action = '#';
+searchForm.method = 'get';
+searchInput.type = 'search';
+searchInput.id = 'search-input';
+searchInput.classList.add('search-input');
+searchInput.placeholder = 'Search...';
+submitBut.type = 'submit';
+submitBut.value = '\u{1F50D}';
+submitBut.id = 'search-submit';
+submitBut.classList.add('search-submit');
+
+searchDiv.appendChild(searchForm);
+searchForm.appendChild(searchInput);
+searchForm.appendChild(submitBut);
 
 const generateCard = (data) => {
   const cardDiv = document.createElement('div');
@@ -31,7 +52,9 @@ const generateCard = (data) => {
   cardInfoContainer.appendChild(emailText);
   cardInfoContainer.appendChild(cityText);
 
-  console.log(data);
+  cardDiv.addEventListener('click', (event) => {
+    console.log(event.target);
+  });
 }
 
 fetch('https://randomuser.me/api/?results=12')
@@ -44,13 +67,27 @@ fetch('https://randomuser.me/api/?results=12')
   }));
 
 
-// <div class="card">
-//     <div class="card-img-container">
-//         <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-//     </div>
-//     <div class="card-info-container">
-//         <h3 id="name" class="card-name cap">first last</h3>
-//         <p class="card-text">email</p>
-//         <p class="card-text cap">city, state</p>
-//     </div>
-// </div>
+
+
+
+  // <div class="modal-container">
+  //     <div class="modal">
+  //         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+  //         <div class="modal-info-container">
+  //             <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+  //             <h3 id="name" class="modal-name cap">name</h3>
+  //             <p class="modal-text">email</p>
+  //             <p class="modal-text cap">city</p>
+  //             <hr>
+  //             <p class="modal-text">(555) 555-5555</p>
+  //             <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+  //             <p class="modal-text">Birthday: 10/21/2015</p>
+  //         </div>
+  //     </div>
+  //
+  //     // IMPORTANT: Below is only for exceeds tasks
+  //     <div class="modal-btn-container">
+  //         <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+  //         <button type="button" id="modal-next" class="modal-next btn">Next</button>
+  //     </div>
+  // </div>
